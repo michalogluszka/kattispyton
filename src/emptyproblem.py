d93e2bda9f60a37b69cca60426559f2222c8bf1b
Michal Ogluszka <michal.ogluszka@avanade.com>
2017-09-27 14:56:48 +0200

    empty solution

--- src/emptyproblem.py
+++ src/emptyproblem.py
@@ -1,20 +1,12 @@
 import sys
 # for tests
 #file = open("1.in") 
+# for submission
 file = sys.stdin
 
 data = file.read().splitlines()
-# for submission
 
 numberOfRows = int(data[0])
 
 for i in range(1,numberOfRows+1):
     line = data[i].split(' ')
-    net = int(line[1]) - int(line[2])
-    r = int(line[0])
-    if(net < r):
-        print("do not advertise")
-    elif(net > r):
-        print("advertise")
-    else:
-        print("does not matter")
