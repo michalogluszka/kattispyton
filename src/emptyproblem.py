e18bd23331d7800275c16a56826cb6cee57c55ec
Michal Ogluszka <michal.ogluszka@avanade.com>
2017-09-27 15:58:48 +0200

    a new alphabet solved

--- src/emptyproblem.py
+++ src/emptyproblem.py
@@ -1,12 +1,45 @@
 import sys
 # for tests
-#file = open("1.in") 
+#file = open("new.in") 
 # for submission
 file = sys.stdin
 
 data = file.read().splitlines()
 
-numberOfRows = int(data[0])
+dic = {'a':'@'
+      ,'b':'8'
+      ,'c':'('
+      ,'d':'|)'
+      ,'e':'3'
+      ,'f':'#'
+      ,'g':'6'
+      ,'h':'[-]'
+      ,'i':'|'
+      ,'j':'_|'
+      ,'k':'|<'
+      ,'l':'1'
+      ,'m':'[]\/[]'
+      ,'n':'[]\[]'
+      ,'o':'0'
+      ,'p':'|D'
+      ,'q':'(,)'
+      ,'r':'|Z'
+      ,'s':'$'
+      ,'t':'\'][\''
+      ,'u':'|_|'
+      ,'v':'\/'
+      ,'w':'\/\/'
+      ,'x':'}{'
+      ,'y':'`/'
+      ,'z':'2'}
 
-for i in range(1,numberOfRows+1):
-    line = data[i].split(' ')
+result = ''
+for x in data[0]:    
+    if x.lower() in dic:
+        result+=dic[x.lower()]
+    else:
+        result+=x
+
+print(result)
+      
+    
