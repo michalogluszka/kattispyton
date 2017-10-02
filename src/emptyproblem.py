c576fb69a619d8a6202d9fcc1a3cacea8eb2051d
Michal Ogluszka <michal.ogluszka@avanade.com>
2017-10-02 15:58:43 +0200

    reading files

--- src/emptyproblem.py
+++ src/emptyproblem.py
@@ -1,12 +1,28 @@
 import sys
 # for tests
-file = open("character-01.in") 
+file = open("sample.in") 
 # for submission
-file = sys.stdin
+#file = sys.stdin
 
-data = file.read().splitlines()
+testCases = int(file.readline().rstrip("\n"))
 
-numberOfRows = int(data[0])
 
-for i in range(1,numberOfRows+1):
-    line = data[i].split(' ')
+for i in range(0,testCases):
+    emptyLine = file.readline().rstrip("\n")
+    
+    number = file.readline().rstrip("\n").split(' ')
+    NG = number[0]    
+    NM = number[1]
+    
+    SG = sorted(list(map(int,file.readline().rstrip("\n").split(' '))))
+    SM = sorted(list(map(int,file.readline().rstrip("\n").split(' '))))
+
+    print(SG)
+    print(SM)
+
+    # if(gozzilla == mecha):
+    #     print("uncertain")
+    # elif(gozzilla < mecha):
+    #     print("MechaGodzilla")
+    # else:
+    #     print("Godzilla")
