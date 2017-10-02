1d9843826836c79335f5615e54b88e5c12cbccd3
Michal Ogluszka <michal.ogluszka@avanade.com>
2017-10-02 14:05:52 +0200

    binary search exceed

--- src/emptyproblem.py
+++ src/emptyproblem.py
@@ -2,49 +2,51 @@
 import sys
 
 # for tests
-# file = open("A.0.in")
+file = open("A.0.in")
 # for submission
-file = sys.stdin
+# file = sys.stdin
 
-data = file.read().splitlines()
+#    Search for number in array
+def binary_search(number, array, lo, hi):
+ 
+    if hi < lo: return -1       # no more numbers
+    mid = (lo + hi) // 2        # midpoint in array
+    if number == array[mid]:
+        return mid                  # number found here
+    elif number < array[mid]:
+        return binary_search(number, array, lo, mid - 1)     # try left of here
+    else:
+        return binary_search(number, array, mid + 1, hi)     # try above here
+ 
+def my_search(int, array):     # convenience interface to binary_search()
+    return binary_search(int, array, 0, len(array) - 1)
 
 counter = 0
 
 while 1:
-    line = data[counter].split(" ")
-
+    
+    line = file.readline().replace('\n','').split(' ')
+    
     canSell = 0
     
-    N = int(line[0])    
+    N = int(line[0])        
     M = int(line[1])
     
     if(N==0 and M==0):
-        break
-
-    counter += 1
+        break    
 
     jacks = []
     jills = []
 
-    for i in range(counter, N + counter):
-        jacks.append(int(data[i]))
+    for i in range(0, N):
+        line = int(file.readline()[0])
+        jacks.append(line)
         counter += 1
 
-    for i in range(counter, M + counter):
-        jills.append(int(data[i]))
-        counter += 1
-
-    i_n = 0
-    i_m = 0
-
-    while(i_n < N and i_m < M):
-        if jacks[i_n] < jills[i_m]:
-            i_n += 1
-        elif jacks[i_n] > jills[i_m]:
-            i_m += 1
-        else:
-            i_n += 1
-            i_m += 1
-            canSell += 1
+    # for i in range(0, M):
+    #     line = int(file.readline()[0])
+    #     if my_search(line,jacks) >= 0:
+    #         canSell+=1              
 
     print(canSell)
+
