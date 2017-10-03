3d934e7bd5b1093cc599ec67ad062400fbe963ea
Michal Ogluszka <michal.ogluszka@avanade.com>
2017-10-03 15:05:16 +0200

    army strength solved

--- src/emptyproblem.py
+++ src/emptyproblem.py
@@ -11,18 +11,35 @@ for i in range(0,testCases):
     emptyLine = file.readline().rstrip("\n")
     
     number = file.readline().rstrip("\n").split(' ')
-    NG = number[0]    
-    NM = number[1]
+    NG = int(number[0])
+    NM = int(number[1])
     
     SG = sorted(list(map(int,file.readline().rstrip("\n").split(' '))))
     SM = sorted(list(map(int,file.readline().rstrip("\n").split(' '))))
 
-    print(SG)
-    print(SM)
+    sg = 0
+    sm = 0
 
-    # if(gozzilla == mecha):
-    #     print("uncertain")
-    # elif(gozzilla < mecha):
-    #     print("MechaGodzilla")
-    # else:
-    #     print("Godzilla")
+    sgEliminated = 0
+    smEliminated = 0
+
+    while sg < NG and sm < NM:
+        if SG[sg] == SM[sm]:
+            smEliminated+=1
+            sm+=1
+        elif SG[sg]>SM[sm]:
+            smEliminated+=1
+            sm+=1
+        elif SG[sg]<SM[sm]:
+            sgEliminated+=1
+            sg+=1    
+
+    sgLeft = NG - sgEliminated
+    ngLeft = NM - smEliminated
+
+    if sgLeft == ngLeft:
+         print("uncertain")
+    elif sgLeft < ngLeft:
+         print("MechaGodzilla")
+    else:
+         print("Godzilla")
