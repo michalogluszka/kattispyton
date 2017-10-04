29cb3926f14093a445e5e8a0d298cde1695b1ce6
Michal Ogluszka <michal.ogluszka@avanade.com>
2017-10-04 11:24:45 +0200

    initial

--- src/emptyproblem.py
+++ src/emptyproblem.py
@@ -1,12 +1,8 @@
 import sys
 # for tests
-file = open("character-01.in") 
+file = open("test0.in") 
 # for submission
-file = sys.stdin
+#file = sys.stdin
 
 data = file.read().splitlines()
 
-numberOfRows = int(data[0])
-
-for i in range(1,numberOfRows+1):
-    line = data[i].split(' ')
