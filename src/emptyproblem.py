450001392b35796ee7eb8785bbb3a9d5bf403a7d
Michal Ogluszka <michal.ogluszka@avanade.com>
2017-10-04 11:46:34 +0200

    left behind solved

--- src/emptyproblem.py
+++ src/emptyproblem.py
@@ -6,3 +6,17 @@ file = open("test0.in")
 
 data = file.read().splitlines()
 
+for line in data:    
+    x = int(line.split(' ')[0])
+    y = int(line.split(' ')[1])
+    if(x==0 and y==0):
+        break
+    
+    if x+y==13:
+        print("Never speak again.")
+    elif x>y:
+        print("To the convention.")
+    elif x<y:
+        print("Left beehind.")
+    elif x==y:
+        print("Undecided.")
