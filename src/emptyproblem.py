e7506d1529d002b1801cc16ab23b44da05fc2e16
Michal Ogluszka <michal.ogluszka@outlook.com>
2017-11-17 23:04:05 +0100

    solved

--- src/emptyproblem.py
+++ src/emptyproblem.py
@@ -1,12 +1,29 @@
 import sys
 # for tests
-file = open("character-01.in") 
+file = open("sample.in") 
 # for submission
-file = sys.stdin
+# file = sys.stdin
+
+def factorial(n):
+    if n == 0:
+        return 1
+    else:
+        return n * factorial(n-1)
 
 data = file.read().splitlines()
 
-numberOfRows = int(data[0])
+for word in data:
+    sort = sorted(word)
+    nf = factorial(len(sort))
 
-for i in range(1,numberOfRows+1):
-    line = data[i].split(' ')
+    distinct = list(set(sort))   
+
+    bottom = 1   
+
+    for l in distinct:
+        x = sort.count(l)
+        bottom = bottom*factorial(x)               
+    result = nf//bottom
+    print(int(result))
+
+
